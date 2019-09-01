c5d1782f94d579f1146df9b7d6ace2bf87a05bda
Joel Cretan <jcretan@gmail.com>
2019-09-01 17:07:49 -0400

    removing old version

--- ios/Pods/Target Support Files/Pods-iOSReactNativeTorDemo-tvOS/Pods-iOSReactNativeTorDemo-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSReactNativeTorDemo_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_iOSReactNativeTorDemo_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-iOSReactNativeTorDemo/Pods-iOSReactNativeTorDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSReactNativeTorDemo : NSObject
-@end
-@implementation PodsDummy_Pods_iOSReactNativeTorDemo
-@end
